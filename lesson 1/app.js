fe308e2da3b247d0fa1d28e28b178e8f35c1d2e2
Nafiur01 <nafiurrahman373@gmail.com>
2025-02-03 22:17:22 +0600

    file handling

---
+++ lesson 1/app.js
@@ -0,0 +1,13 @@
+// console.log(global);
+
+// setTimeout(() => {
+//     console.log("in my country");
+//     clearInterval(inter);
+// }, 3000);
+
+// const inter = setInterval(()=>{
+//     console.log("Bombs Ticking");
+// },1000)
+
+console.log(__dirname);
+console.log(__filename);
---
+++ lesson 1/people.js
@@ -0,0 +1,7 @@
+const people = ["nafiur","rajieb","masruf","sumon"];
+const ages = [35,25,20,15];
+console.log(people);
+
+module.exports = {
+    people,ages
+};
