e775a90ecb375c600bf6f6d6ffaf318a62142098
Nafiur01 <nafiurrahman373@gmail.com>
2025-02-05 22:44:31 +0600

    Request and Response details

--- lesson_2/server.js
+++ lesson_2/server.js
@@ -1,7 +1,40 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.method, req.url);
+
+  // response object
+  res.setHeader("Content-type", "text/html");
+
+  path = "./view/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
